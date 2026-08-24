d72a963002a3be688dfb7238a2e77c9e14a54e4f
kunalraj0212 <razzkunal7@gmail.com>
2026-08-24 18:10:30 +0530

    Add my solution for string slice and replace practice question

---
+++ 03-javascript/02-string-manupulation-and-arrays/practice-questions/03-slice-and-replace.js
@@ -0,0 +1,6 @@
+// Qs. Separate the "College" part in "ApnaCollege" string & replace 'l' with 't' in it.
+
+let str = "ApnaCollege";
+let sliced_part = str.slice(4);
+
+console.log(sliced_part.replace("l", "t"));
