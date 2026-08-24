f713bc9938a7a05ce9f1b3202ff21af4f54bae4e
kunalraj0212 <razzkunal7@gmail.com>
2026-08-24 18:09:00 +0530

    Add my solution for string slice indexOf and replace practice question

---
+++ 03-javascript/02-string-manupulation-and-arrays/practice-questions/02-slice-indexof-replace.js
@@ -0,0 +1,20 @@
+// Qs. For the String -> let name = "ApnaCollege", predict the output for following :
+
+// name.slice(4, 9)
+
+// name.indexOf("na")
+
+// name.replace("Apna","Our")
+
+// Qs. For the String -> let name : "ApnaCollege", predict the output for following :
+
+// name.slice(4, 9)
+
+// name.indexOf("na")
+
+// name.replace("Apna","Our")
+
+let name = "ApnaCollege";
+console.log(name.slice(4, 9)); //Colle
+console.log(name.indexOf("na")); // 2
+console.log(name.replace("Apna", "Our")); //OurCollege
