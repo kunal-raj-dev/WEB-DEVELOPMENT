aae9724b854d70d080f6a24e6071439ea2fc5748
kunalraj0212 <razzkunal7@gmail.com>
2026-09-01 17:28:00 +0530

    Add my demonstration of higher order functions returning function factories

---
+++ 03-javascript/04-functions/15-high-order-return-function.js
@@ -0,0 +1,18 @@
+function oddEvenTest(request) {
+  if (request == "odd") {
+    return function (n) {
+      console.log(!(n % 2 == 0));
+    };
+  } else if (request == "even") {
+    return function (n) {
+      console.log(n % 2 == 0);
+    };
+  } else {
+    console.log("wrong request");
+  }
+}
+
+let request = "odd";
+let test = oddEvenTest(request);
+test(3); // returns true
+
