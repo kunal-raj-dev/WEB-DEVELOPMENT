8d936b36be47e1d6ae75a9508a83f197163d7c8b
kunalraj0212 <razzkunal7@gmail.com>
2026-09-01 17:06:00 +0530

    Add my demonstration of function parameters and arguments in JavaScript

---
+++ 03-javascript/04-functions/04-function-with-argument.js
@@ -0,0 +1,16 @@
+function printInfo(name, age) {
+  console.log(`My name is ${name}. My age is ${age}`);
+}
+
+printInfo();
+printInfo("Parmarth");
+printInfo("Parmarth", 22);
+// printInfo(22, "Parmarth"); // order should be correct
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum();
+sum(2);
+sum(2, 3);
