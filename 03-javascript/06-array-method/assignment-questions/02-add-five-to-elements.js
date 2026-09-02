659e9106d4c93b6edc1a2f46f9963a3b61bd703b
kunalraj0212 <razzkunal7@gmail.com>
2026-09-02 17:36:00 +0530

    Add my solution for adding five to array elements using map method

---
+++ 03-javascript/06-array-method/assignment-questions/02-add-five-to-elements.js
@@ -0,0 +1,8 @@
+// Qs2. Create a new array using the map function whose each element is equal to the original element plus 5.
+
+const nums = [10, 20, 30, 40, 50];
+
+const plusFive = nums.map((num) => num + 5);
+
+console.log("Original Array:", nums);
+console.log("Plus 5 Array:  ", plusFive);
